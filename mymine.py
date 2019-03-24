6ee38260cdf30c088bc5372d58d796dda49aea6a
chaoshunh <38386469+chaoshunh@users.noreply.github.com>
2019-03-24 13:57:43 -0500

    update mymine.py
    change the function name and make it more sense

--- mymine.py
+++ mymine.py
@@ -2,32 +2,29 @@ import requests
 
 from blockchain import BlockChain
 
-
 blockchain = BlockChain()
 
+# --------------------------- Testing The Blockchain Transactions ---------------------------------------------
 
-# --------------------------- Testing The Blockchain Class ---------------------------------------------
-
-
-def print_blockchain(chain):
+def display_blockchain(chain):
     for block in chain:
         print(vars(block))
 
 
-def class_tests():
+def testBlockTransactions():
     print("Length of Current blockchain is: {}".format(len(blockchain.chain)))
-    print_blockchain(blockchain.chain)
+    display_blockchain(blockchain.chain)
 
     blockchain.mine_block('TestFarm', 'AAA', 'Farm', '2')
     print("\nAfter Mining . . .")
     print("Length of Updated blockchain is: {}".format(len(blockchain.chain)))
-    print_blockchain(blockchain.chain)
+    display_blockchain(blockchain.chain)
 
     blockchain.mine_block('TestManufacturer', 'BBB', 'Manufacturer', '3')
     print("\nAfter One more Mining . . .")
     print("Length of Updated blockchain is: {}".format(len(blockchain.chain)))
-    print_blockchain(blockchain.chain)
+    display_blockchain(blockchain.chain)
 
 
 if __name__ == "__main__":
-    class_tests()
+    testBlockTransactions()
